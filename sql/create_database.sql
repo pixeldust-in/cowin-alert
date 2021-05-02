8184adb340b0b6c83ffbcabf02393d80e97c6d93
SiddharthPant <sid@pixeldust.in>
2021-05-02 20:10:52 +0530

    add sql scripts

---
+++ sql/create_database.sql
@@ -0,0 +1,11 @@
+/*
+This file is used to bootstrap development database.
+
+Note: ONLY development database;
+*/
+
+CREATE ROLE cowin_alert SUPERUSER PASSWORD 'devpassword';
+ALTER ROLE vms SET client_encoding TO 'utf8';
+ALTER ROLE vms SET default_transaction_isolation TO 'read committed';
+ALTER ROLE vms SET timezone TO 'UTC';
+CREATE DATABASE cowin_alert OWNER cowin_alert ENCODING 'utf-8';
---
+++ sql/drop_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to drop development database;
+
+Note: ONLY development database;
+*/
+
+DROP DATABASE cowin_alert;
+DROP USER cowin_alert;
