f92b4cc20aa0b63fc70e792cc665ba4df4cdb08b
SiddharthPant <sid@pixeldust.in>
2021-05-02 21:44:39 +0530

    Fix gunicorn and db configs

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -4,8 +4,8 @@ This file is used to bootstrap development database.
 Note: ONLY development database;
 */
 
-CREATE ROLE cowin_alert LOGIN SUPERUSER PASSWORD 'devpassword';
-ALTER ROLE vms SET client_encoding TO 'utf8';
-ALTER ROLE vms SET default_transaction_isolation TO 'read committed';
-ALTER ROLE vms SET timezone TO 'UTC';
+CREATE ROLE cowin_alert LOGIN CREATEDB PASSWORD 'devpassword';
+ALTER ROLE cowin_alert SET client_encoding TO 'utf8';
+ALTER ROLE cowin_alert SET default_transaction_isolation TO 'read committed';
+ALTER ROLE cowin_alert SET timezone TO 'UTC';
 CREATE DATABASE cowin_alert OWNER cowin_alert ENCODING 'utf-8';
